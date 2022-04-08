bffa298885479be635cf2a3b2489eb49f97c9ca2
Kauã Vitor Rodrigues <76789658+kauavitorrodrigues@users.noreply.github.com>
2022-04-08 14:39:29 -0300

    add login css and script

---
+++ navgation/cadastro.js
@@ -0,0 +1,23 @@
+function continuar() {
+    var senha = document.getElementById('input#txtsenha')
+    var nome = document.getElementById('txtnome')
+    var email = document.querySelector('input#txtemail')
+    var endereço = document.querySelector('input#txtend')
+    var tel = document.querySelector('input#txttel')
+    var idade = document.querySelector('input#txtidade')
+    var idade = Number(idade.value)
+    var res = document.querySelector('div#res')
+    
+    if (idade == 0 || idade == '') {
+        alert('Preencha corretamente os dados')
+    }
+
+    else if (idade <= 17) {
+        alert('Não são permitidos menores de idade')
+    }
+
+    else if (idade >= 18){
+        alert('Cadastro concluído, aproveite')
+    }
+
+}
--- navgation/login.js
+++ navgation/login.js
@@ -1,23 +1,13 @@
 function continuar() {
-
-    var nome = document.getElementById('txtnome')
     var email = document.querySelector('input#txtemail')
-    var endereço = document.querySelector('input#txtend')
-    var tel = document.querySelector('input#txttel')
-    var idade = document.querySelector('input#txtidade')
-    var idade = Number(idade.value)
-    var res = document.querySelector('div#res')
+    var senha = document.querySelector('input#txtsenha')
     
-    if (idade == 0 || idade == '') {
+    if ( senha.value == '' || email.value == '') {
         alert('Preencha corretamente os dados')
     }
 
-    else if (idade <= 17) {
-        alert('Não são permitidos menores de idade')
-    }
-
-    else if (idade >= 18){
-        alert('Cadastro concluído, aproveite')
+    else  {
+        alert('Login sucedido')
     }
 
 }
